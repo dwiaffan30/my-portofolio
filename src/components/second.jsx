b7d9d9f28d43ecf7c757e11a04ca3482c150ee3b
C:/Program Files/Git/dwiaffan30 <affandwisaputra9@gmail.com>
2024-08-31 16:12:37 +0700

    Udah explore

---
+++ src/components/second.jsx
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Second = () => {
+    return(
+        <>
+        <Navbar/>
+        <Footer/>
+        </>
+    )
+}
+export default Second;
